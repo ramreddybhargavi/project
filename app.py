8868e21b7f688e69e7d642fab55c6019138767a4
ramreddybhargavi <ramreddybhargavi@gmail.com>
2026-03-16 22:32:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from transformers import pipeline
+
+app = Flask(__name__)
+
+summarizer = pipeline("summarization")
+
+@app.route("/", methods=["GET","POST"])
+def index():
+
+    summary = ""
+    text = ""
+
+    if request.method == "POST":
+        text = request.form["text"]
+
+        result = summarizer(text, max_length=120, min_length=30, do_sample=False)
+
+        summary = result[0]['summary_text']
+
+    return render_template("index.html", summary=summary, text=text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
